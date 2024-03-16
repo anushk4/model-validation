18a5f89f116a76bcf02c76903b7db8c727d20c13
Anushka Srivastava <anushka22086@iiitd.ac.in>
2024-03-17 01:08:13 +0530

    week 2 task 1

--- src/smiles_processing.py
+++ src/smiles_processing.py
@@ -4,31 +4,38 @@ from standardiser import standardise
 
 
 def standardise_smiles(smiles):
-    mols = []
-    for smi in smiles:
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+    except:
+        mol = np.nan
+
+    if mol:
         try:
-            mol = Chem.MolFromSmiles(smi)
+            st_mol = standardise.run(mol)
         except:
-            mol=np.nan
-        mols += [mol]
-    st_mols = []
-    for mol in mols:
-        if mol is not None:
-            try:
-                st_mol = standardise.run(mol)
-            except:
-                st_mol = np.nan
-        else:
             st_mol = np.nan
-        st_mols += [st_mol]
-    st_smiles = []
-    for st_mol in st_mols:
-        if st_mol is not None:
-            try:
-                st_smi = Chem.MolToSmiles(st_mol)
-            except:
-                st_smi=np.nan
-        else:
-            st_smi = np.nan
-        st_smiles += [st_smi]
+
+    if st_mol:
+        st_smiles = Chem.MolToSmiles(mol)
+    else:
+        st_smiles = np.nan
+        
     return st_smiles
+
+def smile_to_inchikey(smiles):
+    try:
+        mol = Chem.MolFromSmiles(smiles)
+    except:
+        mol = np.nan
+
+    if mol:
+        inchi = Chem.inchi.MolToInchi(mol)
+    else:
+        inchi = np.nan
+
+    if inchi:
+        inchi_key = Chem.inchi.InchiToInchiKey(inchi)
+    else:
+        inchi_key = np.nan
+
+    return inchi_key
